da8392b561a1b1a8ead3ee4ec13f60721eee5bc2
Fabian Buentello <fbuentello@mioty.com>
2015-05-25 03:28:32 -0500

    git rid of extend

--- config/config.js
+++ config/config.js
@@ -10,16 +10,14 @@ var development = require('./env/development');
 var test = require('./env/test');
 var production = require('./env/production');
 
-var defaults = {
-  root: path.normalize(__dirname + '/..')
-};
 
 /**
  * Expose
  */
 
 module.exports = {
-  development: extend(development, defaults),
-  test: extend(test, defaults),
-  production: extend(production, defaults)
+  development: development,
+  test: test,
+  production: production,
+  root: path.normalize(__dirname + '/..')
 }[process.env.NODE_ENV || 'development'];
