fb93c1035ba49bc9baf0175ed50f76474692cff6
Fabian Buentello <fbuentello@mioty.com>
2015-05-27 06:03:10 -0500

    test(additional test): add post and delete tests

--- test/attendeeSpec.js
+++ test/attendeeSpec.js
@@ -4,19 +4,58 @@ var should = require('should'),
 request = require('supertest'),
 _ = require('lodash');
 
-
+var testAttendeeID = '';
 
 describe('Attendee', function() {
 	this.timeout(15000);
+	it('should create a new attendee(testAttendee) in the database', function(done) {
 
-	it('should return valid attendee for attendeeID: 555ceac57039569010fa5040', function(done) {
+
+		var newAttendee = {
+			firstName: 'testFirstName',
+			lastName: 'testLastName',
+			email: 'testEmail@test.com',
+			school: 'testSchool',
+			links: {
+				resume: 'testResume.com',
+				github: 'testGithub.com'
+			},
+			references: 'testReferences',
+			transportation: 'Driving',
+			firstHackathon: true,
+			health_Medical_BioTech_Hack: false,
+			usingHardware: true
+		};
+
 		request(app)
-		.get('/attendees/555ceac57039569010fa5040')
+		.post('/attendees')
+		.send(newAttendee)
 		.expect(200)
 		.end(function(err, res) {
-			res.status.should.equal(200);
+			if (err) {
+				throw res.body.message;
+			} else {
+				res.status.should.equal(200);
+				res.body.should.not.equal(undefined);
+				testAttendeeID = res.body._id;
+			}
+			done();
 		});
-		done();
+	});
+
+	it('should return our testAttendee', function(done) {
+		request(app)
+		.get('/attendees/'+testAttendeeID)
+		.expect(200)
+		.end(function(err, res) {
+			if (err) {
+				throw res.body.message;
+			} else {
+				res.status.should.equal(200);
+				res.body.firstName.should.equal('testFirstName');
+			}
+			done();
+		});
 	});
 
 	it('should return an error for an invalid attendeeID: 999999999', function(done) {
@@ -29,10 +68,10 @@ describe('Attendee', function() {
 		});
 	});
 
-	it('should mark Attendee as isComing', function(done) {
+	it('should mark our testAttendee.isComing as True', function(done) {
 
 		request(app)
-		.post('/attendees/555ceac57039569010fa5040')
+		.post('/attendees/'+testAttendeeID)
 		.send({isComing: true})
 		.expect(200)
 		.end(function(err, res) {
@@ -43,15 +82,45 @@ describe('Attendee', function() {
 				res.status.should.equal(200);
 				res.body.should.not.equal(undefined);
 				request(app)
-				.get('/attendees/555ceac57039569010fa5040')
+				.get('/attendees/'+testAttendeeID)
 				.expect(200)
 				.end(function(err, res) {
-					res.status.should.equal(200);
-					res.body.isComing.should.equal(true);
+					if (err) {
+						throw res.body.message;
+					} else {
+						res.status.should.equal(200);
+						res.body.isComing.should.equal(true);
+					}
 					done();
 				});
 			}
+		});
+	});
 
+	it('should delete our testAttendee', function(done) {
+		request(app)
+		.delete('/attendees/'+testAttendeeID)
+		.expect(200)
+		.end(function(err, res) {
+			if (err) {
+				throw res.body.message;
+			} else {
+				res.status.should.equal(200);
+				res.body.status.should.equal('deleted!');
+			}
+			done();
+		});
+	});
+
+	it('should return an empty list of Attendees', function(done) {
+		request(app)
+		.get('/attendees')
+		.expect(200)
+		.end(function(err, res) {
+			console.log('body: ', res.body);
+			res.status.should.equal(200);
+			res.body.length.should.equal(0);
+			done();
 		});
 	});
 });
