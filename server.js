521862200eb488e420b53f1c2ab26ade69f5b758
Fabian Buentello <fbuentello@mioty.com>
2015-08-11 12:58:50 -0500

    add error handler in server.js

--- server.js
+++ server.js
@@ -5,6 +5,11 @@
 var config = require('./config/config'),
 	db = require('./config/db'),
 	app = require('./app.js')(config, db);
+/// Error Handler
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.send(err);
+});
 var port = process.env.PORT || 3000;
 app.listen(port);
 console.log('Express app started on port ' + port);
