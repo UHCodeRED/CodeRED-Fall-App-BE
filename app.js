c55d4077eb224c925451c52bb359f13d34816a95
Fabian Buentello <fbuentello@mioty.com>
2015-09-16 00:15:13 -0500

    attendees can accept invitation

--- app.js
+++ app.js
@@ -5,16 +5,30 @@
 var fs = require('fs');
 var app = require('express')();
 var passport = require('passport');
+var env = process.env.NODE_ENV || 'development';
+var methodOverride = require('method-override');
 
 module.exports = function(config, db) {
 
 	app.use(function(req, res, next) {
-		res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+		if (env === 'development' || env === 'test') {
+			res.header("Access-Control-Allow-Origin", "*");
+		}
+		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 		next();
 	});
 	app.options('*', function(req, res) {
-		res.send(200);
+		res.sendStatus(200);
 	});
+
+	app.use(methodOverride(function (req, res) {
+		if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+			// look in urlencoded POST bodies and delete it
+			var method = req.body._method;
+			delete req.body._method;
+			return method;
+		}
+	}));
 	db(config);
 
 
--- config/express.js
+++ config/express.js
@@ -1,103 +1,103 @@
 
 /**
- * Module dependencies.
- */
+* Module dependencies.
+*/
 
- var express = require('express');
- var session = require('express-session');
- var compression = require('compression');
- var morgan = require('morgan');
- var cookieParser = require('cookie-parser');
- var cookieSession = require('cookie-session');
- var bodyParser = require('body-parser');
- var csrf = require('csurf');
- var swig = require('swig');
- var path = require('path');
- var mongoStore = require('connect-mongo')(session);
- var flash = require('connect-flash');
- var winston = require('winston');
- var helpers = require('view-helpers');
- var config = require('./config');
- var pkg = require('../package.json');
+var express = require('express');
+var session = require('express-session');
+var compression = require('compression');
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var cookieSession = require('cookie-session');
+var bodyParser = require('body-parser');
+var csrf = require('csurf');
+var swig = require('swig');
+var path = require('path');
+var mongoStore = require('connect-mongo')(session);
+var flash = require('connect-flash');
+var winston = require('winston');
+var helpers = require('view-helpers');
+var config = require('./config');
+var pkg = require('../package.json');
 
- var env = process.env.NODE_ENV || 'development';
+var env = process.env.NODE_ENV || 'development';
 
 /**
- * Expose
- */
+* Expose
+*/
 
- module.exports = function (app, passport) {
+module.exports = function (app, passport) {
 
-  // Compression middleware (should be placed before express.static)
-  app.use(compression({
-    threshold: 512
-  }));
+	// Compression middleware (should be placed before express.static)
+	app.use(compression({
+		threshold: 512
+	}));
 
-  // Static files middleware
-  app.use(express.static(path.join(__dirname, '../public')));
+	// Static files middleware
+	app.use(express.static(path.join(__dirname, '../public')));
 
-  // Use winston on production
-  var log;
-  if (env !== 'development') {
-    log = {
-      stream: {
-        write: function (message, encoding) {
-          winston.info(message);
-        }
-      }
-    };
-  } else {
-    log = 'dev';
-  }
+	// Use winston on production
+	var log;
+	if (env !== 'development') {
+		log = {
+			stream: {
+				write: function (message, encoding) {
+					winston.info(message);
+				}
+			}
+		};
+	} else {
+		log = 'dev';
+	}
 
-  // Don't log during tests
-  // Logging middleware
-//  if (env !== 'test') app.use(morgan('default', log));
+	// Don't log during tests
+	// Logging middleware
+	//  if (env !== 'test') app.use(morgan('default', log));
 
-  // Swig templating engine settings
-  if (env === 'development' || env === 'test') {
-    swig.setDefaults({
-      cache: false
-    });
-  }
+	// Swig templating engine settings
+	if (env === 'development' || env === 'test') {
+		swig.setDefaults({
+			cache: false
+		});
+	}
 
-  // set views path, template engine and default layout
-  app.engine('html', swig.renderFile);
-  app.set('views', './app/views');
-  app.set('view engine', 'html');
+	// set views path, template engine and default layout
+	app.engine('html', swig.renderFile);
+	app.set('views', './app/views');
+	app.set('view engine', 'html');
 
-  // expose package.json to views
-  app.use(function (req, res, next) {
-    res.locals.pkg = pkg;
-    res.locals.env = env;
-    next();
-  });
+	// expose package.json to views
+	app.use(function (req, res, next) {
+		res.locals.pkg = pkg;
+		res.locals.env = env;
+		next();
+	});
 
-  // bodyParser should be above methodOverride
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({extended: false }));
-  // cookieParser should be above session
-  app.use(cookieParser());
-  app.use(cookieSession({ secret: process.env.cookieSessionSecret || 'secret' }));
-  app.use(session({
-    secret: pkg.name,
-    proxy: true,
-    resave: true,
-    saveUninitialized: true,
-    store: new mongoStore({
-      url: config.db,
-      collection : 'sessions'
-    })
-  }));
+	// bodyParser should be above methodOverride
+	app.use(bodyParser.json());
+	app.use(bodyParser.urlencoded({extended: false }));
+	// cookieParser should be above session
+	app.use(cookieParser());
+	app.use(cookieSession({ secret: process.env.cookieSessionSecret || 'secret' }));
+	app.use(session({
+		secret: pkg.name,
+		proxy: true,
+		resave: true,
+		saveUninitialized: true,
+		store: new mongoStore({
+			url: config.db,
+			collection : 'sessions'
+		})
+	}));
 
-  // use passport session
-  app.use(passport.initialize());
-  app.use(passport.session());
+	// use passport session
+	app.use(passport.initialize());
+	app.use(passport.session());
 
-  // connect flash for flash messages - should be declared after sessions
-  app.use(flash());
+	// connect flash for flash messages - should be declared after sessions
+	app.use(flash());
 
-  // should be declared after session and flash
-  app.use(helpers(pkg.name));
+	// should be declared after session and flash
+	app.use(helpers(pkg.name));
 
 };
